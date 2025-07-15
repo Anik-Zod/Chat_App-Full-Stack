464ae7f79e5139588f75b817b75bded6bbed2e31
Anik Zod <anikdas169@gmail.com>
2025-07-15 20:48:02 +0600

    fronted url added to Backend

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chat-app-full-stack-u4xj.vercel.app",
     credentials:true
 }))
 
